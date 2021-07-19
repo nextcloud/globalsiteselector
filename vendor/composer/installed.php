cb9149b3862f4d6874f92326038efd3f5faf9188
Julius Härtl <jus@bitgrid.net>
2021-07-19 20:39:58 +0200

    bump composer files
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,32 +1,33 @@
-<?php return array(
-    'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => '7e94245461b1515be3dd20c9d855ae407a927e02',
-        'name' => '__root__',
-        'dev' => false,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => '7e94245461b1515be3dd20c9d855ae407a927e02',
-            'dev_requirement' => false,
-        ),
-        'firebase/php-jwt' => array(
-            'pretty_version' => 'v4.0.0',
-            'version' => '4.0.0.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../firebase/php-jwt',
-            'aliases' => array(),
-            'reference' => 'dccf163dc8ed7ed6a00afc06c51ee5186a428d35',
-            'dev_requirement' => false,
-        ),
+    'reference' => '19320dd2784485312c35ccdf74052e23e8e954be',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '19320dd2784485312c35ccdf74052e23e8e954be',
     ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dccf163dc8ed7ed6a00afc06c51ee5186a428d35',
+    ),
+  ),
 );
