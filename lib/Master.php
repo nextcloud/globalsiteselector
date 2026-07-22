1840201600f6221ef5f3cb9fdebd95a326b698c5
Carl Schwan <carl@carlschwan.eu>
2026-07-22 17:43:02 +0200

    fix: run composer cs:fix
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- lib/Master.php
+++ lib/Master.php
@@ -13,7 +13,6 @@ use Exception;
 use OC\Core\Controller\ClientFlowLoginV2Controller;
 use OC\Core\Service\LoginFlowV2Service;
 use OCA\GlobalSiteSelector\AppInfo\Application;
-use OCA\GlobalSiteSelector\ConfigLexicon;
 use OCA\GlobalSiteSelector\UserDiscoveryModules\IUserDiscoveryModule;
 use OCA\GlobalSiteSelector\Vendor\Firebase\JWT\JWT;
 use OCA\GlobalSiteSelector\Vendor\Firebase\JWT\Key;
@@ -50,7 +49,7 @@ class Master {
 		private readonly Lookup $lookup,
 		private readonly IRequest $request,
 		private readonly IClientService $clientService,
-        private readonly IAppConfig $appConfig,
+		private readonly IAppConfig $appConfig,
 		private readonly IConfig $config,
 		private readonly LoggerInterface $logger,
 	) {
@@ -273,9 +272,9 @@ class Master {
 		$isDirectWebDavAccess = strpos($requestUri, 'remote.php/webdav') !== false;
 		$isDirectWebDavAccess = $isDirectWebDavAccess || strpos($requestUri, 'remote.php/dav') !== false;
 
-        $authHeader = $this->request->getHeader('Authorization');
-        $redirectWebDav = $this->appConfig->getValueBool(Application::APP_ID, ConfigLexicon::REDIRECT_WEBDAV);
-        $hasBasicAuth = $redirectWebDav && $authHeader !== '' && str_starts_with(strtolower($authHeader), 'basic ');
+		$authHeader = $this->request->getHeader('Authorization');
+		$redirectWebDav = $this->appConfig->getValueBool(Application::APP_ID, ConfigLexicon::REDIRECT_WEBDAV);
+		$hasBasicAuth = $redirectWebDav && $authHeader !== '' && str_starts_with(strtolower($authHeader), 'basic ');
 
 		// default redirect status code; overridden below for the 307 forward.
 		$statusCode = 302;
